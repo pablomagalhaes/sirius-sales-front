a1ef71bc17f5dda394c626391ff6d0ab8ef78763
Christopher De Oliveira Souza <christophersouza.partner@fiorde.com.br>
2021-12-20 12:48:59 -0300

    SIRPLT-778 fixing eslint issues

--- src/presentation/components/CostModal/CostModal.test.tsx
+++ src/presentation/components/CostModal/CostModal.test.tsx
@@ -6,7 +6,6 @@ import CostModal from './CostModal'
 
 import '@testing-library/jest-dom'
 
-
 const props = {
 
   dataProp: {
@@ -22,63 +21,44 @@ const props = {
   },
 
   title: 'Editar item',
-
   handleAdd: () => {},
-
   setOpen: () => {},
-
   open: true,
-
   setClose: () => {}
-
 }
 
 test('should show text Editar item', () => {
-
   render(<CostModal {...props} />)
-
   expect(screen.getByText(/Editar item/).textContent).toBe('Editar item')
-
 })
 
 test('should click on the Close Icon and should trigger setClose', () => {
-
   const handleClose = jest.fn()
-
   render(<CostModal {...props} setClose={handleClose} />)
-
   fireEvent.click(screen.getByTitle(/Close Icon/))
-
   expect(handleClose).toBeCalledTimes(1)
-
 })
 
 test('should click on the save button and should trigger handleAdd', () => {
-
   const handleAdd = jest.fn()
-
   render(<CostModal {...props} handleAdd={handleAdd} />)
-
   fireEvent.click(screen.getByText(/save/))
-
   expect(handleAdd).toBeCalledTimes(1)
-
 })
 
 test('should click on the checkbox and show the check icon', () => {
-    render(<CostModal {...props} />)
-    fireEvent.click(screen.getByLabelText('checkbox'))
-    expect(screen.getByTitle('Check Icon')).toBeInTheDocument()
+  render(<CostModal {...props} />)
+  fireEvent.click(screen.getByLabelText('checkbox'))
+  expect(screen.getByTitle('Check Icon')).toBeInTheDocument()
 })
 
-
 test('should click on the checkbox and click on the save button and should not trigger handleAdd due invalid input', () => {
-    const jsdomAlert = window.alert
-    window.alert = () => {}
-    const handleAdd = jest.fn()
-    render(<CostModal {...props} handleAdd={handleAdd} />)
-    fireEvent.click(screen.getByLabelText('checkbox'))
-    fireEvent.click(screen.getByText(/save/))
-    expect(handleAdd).toBeCalledTimes(0)
-    window.alert = jsdomAlert
+  const jsdomAlert = window.alert
+  window.alert = () => {}
+  const handleAdd = jest.fn()
+  render(<CostModal {...props} handleAdd={handleAdd} />)
+  fireEvent.click(screen.getByLabelText('checkbox'))
+  fireEvent.click(screen.getByText(/save/))
+  expect(handleAdd).toBeCalledTimes(0)
+  window.alert = jsdomAlert
 })
