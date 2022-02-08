b9633543b74c8a99d74d5c7b99c5fa97c25192f9
Camila Oliveira <camilaoliveira.partner@fiorde.com.br>
2022-02-08 12:18:28 -0300

    SIRPLT-862 Validate Step1 fields and set step as completed.

--- src/presentation/pages/NewProposal/steps/Step1.tsx
+++ src/presentation/pages/NewProposal/steps/Step1.tsx
@@ -5,28 +5,42 @@ import {
   FormLabel,
   Grid,
   InputAdornment,
-  Radio,
   RadioGroup
 } from '@material-ui/core/'
 import { I18n } from 'react-redux-i18n'
-import { Title, Subtitle, IconContainer, Separator } from '../style'
+import { Title, Subtitle, IconContainer, Separator, StyledRadio } from '../style'
 import IconComponent from '../../../../application/icons/IconComponent'
 import { withTheme } from 'styled-components'
 import ControlledInput from '../../../components/ControlledInput'
 import { RedColorSpan } from '../../../components/StyledComponents/modalStyles'
+import { useEffect } from 'react'
 export interface Step1Props {
-  theme: any,
-  completed: boolean,
+  theme?: any
   invalidInput: boolean
+  setCompleted: (completed: any) => void
 }
 
-const Step1 = ({ theme, completed, invalidInput }: Step1Props): JSX.Element => {
+const Step1 = ({ theme, invalidInput, setCompleted }: Step1Props): JSX.Element => {
   const [data, setData] = useState({
     proposal: '',
     services: '',
-    client: '',
+    proposalValue: '',
     modal: ''
   })
+
+  useEffect(() => {
+    if (data.proposal !== '' && data.proposalValue !== '' && data.modal !== '') {
+      setCompleted((currentState) => ({ ...currentState, step1: true }))
+    } else {
+      setCompleted((currentState) => ({ ...currentState, step1: false }))
+    }
+  }, [data])
+
+  const getColor = (value) => {
+    if (value === '' && invalidInput) {
+      return theme?.commercial?.components?.itemModal?.redAsterisk;
+    }
+  }
 
   return (
     <Separator>
@@ -43,8 +57,8 @@ const Step1 = ({ theme, completed, invalidInput }: Step1Props): JSX.Element => {
             value={data.proposal}
             onChange={e => setData({ ...data, proposal: e.target.value })}
           >
-            <FormControlLabel value="client" control={<Radio />} label={I18n.t('pages.newProposal.step1.client')} />
-            <FormControlLabel value="routing" control={<Radio />} label={I18n.t('pages.newProposal.step1.routingOrder')} />
+            <FormControlLabel value="client" control={<StyledRadio color={getColor(data.proposal)} />} label={I18n.t('pages.newProposal.step1.client')} />
+            <FormControlLabel value="routing" control={<StyledRadio color={getColor(data.proposal)} />} label={I18n.t('pages.newProposal.step1.routingOrder')} />
           </RadioGroup>
         </Grid>
         {
@@ -67,11 +81,11 @@ const Step1 = ({ theme, completed, invalidInput }: Step1Props): JSX.Element => {
           <ControlledInput
             id="search-client"
             toolTipTitle={I18n.t('components.itemModal.requiredField')}
-            invalid={false}
+            invalid={data.proposalValue === '' && invalidInput}
             variant="outlined"
             size="small"
-            onChange={e => setData({ ...data, client: e.target.value })}
-            value={data.client}
+            onChange={e => setData({ ...data, proposalValue: e.target.value })}
+            value={data.proposalValue}
             placeholder={I18n.t('pages.newProposal.step1.searchClient')}
             InputProps={{
               endAdornment: (
@@ -85,15 +99,15 @@ const Step1 = ({ theme, completed, invalidInput }: Step1Props): JSX.Element => {
       </Grid>
       <FormLabel component="legend">{I18n.t('pages.newProposal.step1.modal')}<RedColorSpan> *</RedColorSpan></FormLabel>
       <RadioGroup row aria-label="modal" name="row-radio-buttons-group" onChange={e => setData({ ...data, modal: e.target.value })}>
-        <FormControlLabel value="1" control={<Radio />} label={I18n.t('pages.newProposal.step1.air')} />
+        <FormControlLabel value="1" control={<StyledRadio color={getColor(data.modal)} />} label={I18n.t('pages.newProposal.step1.air')} />
         <IconContainer>
           <IconComponent name="plane" defaultColor={theme?.commercial?.pages?.newProposal?.subtitle} />
         </IconContainer>
-        <FormControlLabel value="2" control={<Radio />} label={I18n.t('pages.newProposal.step1.marine')} />
+        <FormControlLabel value="2" control={<StyledRadio color={getColor(data.modal)} />} label={I18n.t('pages.newProposal.step1.marine')} />
         <IconContainer>
           <IconComponent name="ship" defaultColor={theme?.commercial?.pages?.newProposal?.subtitle} />
         </IconContainer>
-        <FormControlLabel value="3" control={<Radio />} label={I18n.t('pages.newProposal.step1.road')} />
+        <FormControlLabel value="3" control={<StyledRadio color={getColor(data.modal)} />} label={I18n.t('pages.newProposal.step1.road')} />
         <IconContainer>
           <IconComponent name="truck" defaultColor={theme?.commercial?.pages?.newProposal?.subtitle} />
         </IconContainer>
