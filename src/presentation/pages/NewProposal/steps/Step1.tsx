a87aa1768f367cd956001150d67ad2417ffc78ce
Fernando Becker <fernandosilva.partner@fiorde.com.br>
2022-02-23 11:07:18 -0300

    SIRPLT-771 Revert

--- src/presentation/pages/NewProposal/steps/Step1.tsx
+++ src/presentation/pages/NewProposal/steps/Step1.tsx
@@ -41,15 +41,15 @@ const Step1 = ({ theme, invalidInput, setCompleted, setProposalType, setModal }:
   }, [])
 
   useEffect(() => {
-    setProposalType(data?.proposal)
-  }, [data?.proposal])
+    setProposalType(data.proposal)
+  }, [data.proposal])
 
   useEffect(() => {
-    setModal(data?.modal)
-  }, [data?.modal])
+    setModal(data.modal)
+  }, [data.modal])
 
   useEffect(() => {
-    if (data?.proposal !== '' && data?.proposalValue !== '' && data?.modal !== '') {
+    if (data.proposal !== '' && data.proposalValue !== '' && data.modal !== '') {
       setCompleted((currentState) => ({ ...currentState, step1: true }))
     } else {
       setCompleted((currentState) => ({ ...currentState, step1: false }))
@@ -74,18 +74,18 @@ const Step1 = ({ theme, invalidInput, setCompleted, setProposalType, setModal }:
           <RadioGroup
             row aria-label="proposal type"
             name="row-radio-buttons-group"
-            value={data?.proposal}
-            onChange={e => setData({ data, proposal: e.target.value })}
+            value={data.proposal}
+            onChange={e => setData({ ...data, proposal: e.target.value })}
           >
-            <FormControlLabel value="client" control={<StyledRadio color={getColor(data?.proposal)} />} label={I18n.t('pages.newProposal.step1.client')} />
-            <FormControlLabel value="routing" control={<StyledRadio color={getColor(data?.proposal)} />} label={I18n.t('pages.newProposal.step1.routingOrder')} />
+            <FormControlLabel value="client" control={<StyledRadio color={getColor(data.proposal)} />} label={I18n.t('pages.newProposal.step1.client')} />
+            <FormControlLabel value="routing" control={<StyledRadio color={getColor(data.proposal)} />} label={I18n.t('pages.newProposal.step1.routingOrder')} />
           </RadioGroup>
         </Grid>
         {
-          data?.proposal === 'routing'
+          data.proposal === 'routing'
             ? <Grid item xs={6}>
               <FormLabel component="legend">{I18n.t('pages.newProposal.step1.services')}</FormLabel>
-              <RadioGroup row aria-label="services" name="row-radio-buttons-group" onChange={e => setData({ data, services: e.target.value })}>
+              <RadioGroup row aria-label="services" name="row-radio-buttons-group" onChange={e => setData({ ...data, services: e.target.value })}>
                 <FormControlLabel value="transport" control={<Checkbox />} label={I18n.t('pages.newProposal.step1.transport')} />
                 <FormControlLabel value="desemb" control={<Checkbox />} label={I18n.t('pages.newProposal.step1.readiness')} />
               </RadioGroup>
@@ -94,18 +94,18 @@ const Step1 = ({ theme, invalidInput, setCompleted, setProposalType, setModal }:
         }
         <Grid item xs={6}>
           {
-            data?.proposal === 'routing'
+            data.proposal === 'routing'
               ? <FormLabel component="legend">{I18n.t('pages.newProposal.step1.agents')}<RedColorSpan> *</RedColorSpan></FormLabel>
               : <FormLabel component="legend">{I18n.t('pages.newProposal.step1.client')}:<RedColorSpan> *</RedColorSpan></FormLabel>
           }
           <ControlledInput
             id="search-client"
             toolTipTitle={I18n.t('components.itemModal.requiredField')}
-            invalid={data?.proposalValue === '' && invalidInput}
+            invalid={data.proposalValue === '' && invalidInput}
             variant="outlined"
             size="small"
             onChange={e => setData({ ...data, proposalValue: e.target.value })}
-            value={data?.proposalValue}
+            value={data.proposalValue}
             placeholder={I18n.t('pages.newProposal.step1.searchClient')}
             InputProps={{
               endAdornment: (
@@ -118,12 +118,12 @@ const Step1 = ({ theme, invalidInput, setCompleted, setProposalType, setModal }:
         </Grid>
       </Grid>
       <FormLabel component="legend">{I18n.t('pages.newProposal.step1.modal')}<RedColorSpan> *</RedColorSpan></FormLabel>
-      <RadioGroup row aria-label="modal" name="row-radio-buttons-group" onChange={e => setData({ data, modal: e.target.value })}>
+      <RadioGroup row aria-label="modal" name="row-radio-buttons-group" onChange={e => setData({ ...data, modal: e.target.value })}>
         {transportList.map((item) => (
           <>
             <FormControlLabel
               value={item.id}
-              control={<StyledRadio color={getColor(data?.modal)} />}
+              control={<StyledRadio color={getColor(data.modal)} />}
               label={item.description}
             />
             <IconContainer>
