dc1be4e18f56cad848c9e755604f6a1a01ffd454
Thiago Magalhães <pablo.magalhaes@infobase.com.br>
2023-06-28 16:10:10 -0300

    feature/SIRPLT-1429 - Ajuste na lib de teste unitario

--- src/presentation/pages/Tariff/TariffProcessingTable.Test.tsx
+++ src/presentation/pages/Tariff/TariffProcessingTable.Test.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import { render } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
+import { render, fireEvent, screen } from '@testing-library/react'
 import TariffProcessingTable, { TariffProcessingTableProps } from './TariffProcessingTable'
+import '@testing-library/jest-dom'
 
-describe('TariffProcessingTable', () => {
+test('should render the table row data correctly', () => {
   const rows = [
     {
       idUploadFile: 'abcde12345',
@@ -19,20 +19,32 @@ describe('TariffProcessingTable', () => {
   const handleSelectedItem = jest.fn()
   const defaultProps: TariffProcessingTableProps = { rows, handleSelectedItem }
 
-  it('should render the table row data correctly', () => {
-    const { getByText } = render(<TariffProcessingTable {...defaultProps} />)
-    expect(getByText('testFile.csv')).toBeInTheDocument()
-    expect(getByText('01/10/2021 12:00')).toBeInTheDocument()
-    expect(getByText('Test User')).toBeInTheDocument()
-    expect(getByText('High')).toBeInTheDocument()
-    expect(getByText('Rail')).toBeInTheDocument()
-    expect(getByText('Sucess')).toBeInTheDocument()
-  })
+  const { getByText } = render(<TariffProcessingTable {...defaultProps} />)
+  expect(getByText('testFile.csv')).toBeInTheDocument()
+  expect(getByText('01/10/2021 12:00')).toBeInTheDocument()
+  expect(getByText('Test User')).toBeInTheDocument()
+  expect(getByText('High')).toBeInTheDocument()
+  expect(getByText('Rail')).toBeInTheDocument()
+  expect(getByText('Sucess')).toBeInTheDocument()
+})
 
-  it('should call handleSelectedItem when a row is clicked', () => {
-    const { getByText } = render(<TariffProcessingTable {...defaultProps} />)
-    userEvent.click(getByText('Sucess'))
-    expect(handleSelectedItem).toHaveBeenCalled()
-  })
+test('should call handleSelectedItem when a row is clicked', () => {
+  const rows = [
+    {
+      idUploadFile: 'abcde12345',
+      txFileName: 'testFile.csv',
+      dtProcess: new Date('2021-10-01T12:00:00Z'),
+      userCreation: 'Test User',
+      tariffType: 'high',
+      tariffModalType: 'rail',
+      nmAgent: null,
+      txStatus: 'success'
+    }
+  ]
+  const handleSelectedItem = jest.fn()
+  const defaultProps: TariffProcessingTableProps = { rows, handleSelectedItem }
 
+  render(<TariffProcessingTable {...defaultProps} />)
+  fireEvent.click(screen.getByText('Sucess'))
+  expect(handleSelectedItem).toHaveBeenCalled()
 })
