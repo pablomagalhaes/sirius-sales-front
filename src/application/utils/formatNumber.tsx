00d77cb8a1bdc7b131892e1ea1039bda62582655
Mario Costa <mariocosta@fiorde.com.br>
2023-07-05 10:13:57 -0300

    SIRPLT-1538 code review

--- src/application/utils/formatNumber.tsx
+++ src/application/utils/formatNumber.tsx
@@ -11,13 +11,13 @@ const rightToLeftFormatter = (value: string, decimal: number): string => {
   return String(amount).replace('.', ',')
 }
 
-const convertNumberToString = (value: number | undefined): string => {
-  if(value !== null) return String(value?.toFixed(2)).replace('.', ',')
-  return null
+const convertNumberToString = (value: number | undefined | null): string | null => {
+  if(value !== null && value !== undefined) return String(value?.toFixed(2)).replace('.', ',')
+  return null 
 }
 
-const convertStringToNumber = (value: string | undefined): number => {
-  if(value !== null) return Number(value?.replace(',', '.'))
+const convertStringToNumber = (value: string | undefined | null): number | null => {
+  if(value !== null && value !== undefined) return Number(value?.replace(',', '.'))
   return null
 }
 
