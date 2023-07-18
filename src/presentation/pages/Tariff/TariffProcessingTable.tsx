b70a1276d4eb42f2ce444353014a821b36c58a72
Thiago Magalhães <pablo.magalhaes@infobase.com.br>
2023-07-18 13:39:59 -0300

    bugfix/SIRPLT-1554 - Ajuste na paginacao

--- src/presentation/pages/Tariff/TariffProcessingTable.tsx
+++ src/presentation/pages/Tariff/TariffProcessingTable.tsx
@@ -18,116 +18,74 @@ export interface TariffProcessingTableProps {
   handleSelectedItem: (id: any) => void
 }
 
-const defaultFilter = {
-  direction: 'ASC',
-  orderByList: 'tariffType',
-  page: 0,
-  size: 10,
-  tariffModalType: '',
-  validityTariff: '',
-  tariffType: ''
-}
-
 const TariffProcessingTable = ({
   rows,
   handleSelectedItem
 }: TariffProcessingTableProps): JSX.Element => {
   const [state, setState] = useState({ anchorEl: null, currentKey: null })
-  const [filter, setFilter] = useState<any>(defaultFilter)
-  const [totalFilesList, setTotalFilesList] = useState<number>(1)
 
   const handleClick = (event: any, key: any, id: any): void => {
     setState({ anchorEl: event.currentTarget, currentKey: key })
     handleSelectedItem(id)
   }
 
-  useEffect(() => {
-    setTotalFilesList(rows.length)
-  },[rows])
-
   return (
     <>
-      <TableContainer>
-        <Table>
-          <TableHeader>
-            <TableRow>
-              <TableCell>Nome do arquivo</TableCell>
-              <TableCell>Data/hora do processamento</TableCell>
-              <TableCell>Usuário</TableCell>
-              <TableCell>Tipo</TableCell>
-              <TableCell>Modal</TableCell>
-              <TableCell>Agente</TableCell>
-              <TableCell>Status do processamento</TableCell>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableCell>Nome do arquivo</TableCell>
+            <TableCell>Data/hora do processamento</TableCell>
+            <TableCell>Usuário</TableCell>
+            <TableCell>Tipo</TableCell>
+            <TableCell>Modal</TableCell>
+            <TableCell>Agente</TableCell>
+            <TableCell>Status do processamento</TableCell>
+          </TableRow>
+        </TableHeader>
+        <TableBod>
+          {rows.map((row, index) => (
+            <TableRow key={index}>
+              <TableCell>{row.txFileName}</TableCell>
+              <TableCell>
+                {moment(row.dtProcess).format('DD/MM/YYYY HH:MM')}
+              </TableCell>
+              <TableCell>{row.userCreation}</TableCell>
+              <TableCell>{capitalizeFirstLetter(row.tariffType)}</TableCell>
+              <TableCell>{capitalizeFirstLetter(row.tariffModalType)}</TableCell>
+              {/* eslint-disable-next-line @typescript-eslint/strict-boolean-expressions */}
+              <TableCell>{row.nmAgent && capitalizeFirstLetter(row.nmAgent)}</TableCell>
+              <TableCell>
+                <div
+                  style={{
+                    borderRadius: '50%',
+                    height: '15px',
+                    width: '15px',
+                    float: 'left',
+                    marginRight: '5px',
+                    backgroundColor: `${chooseStatusColor(row.txStatus)}`
+                  }}>
+                </div>
+                <div
+                  style={{
+                    lineHeight: '16px',
+                    marginLeft: '10px',
+                    cursor: 'pointer'
+                  }}
+                  onClick={
+                    row.txStatus ===
+                    I18n.t('pages.tariff.tariffTableProcessing.sucessLabel')
+                      ? () => {}
+                      : (e) => handleClick(e, index, row.idUploadFile)
+                  }
+                >
+                  {capitalizeFirstLetter(row.txStatus)}
+                </div>
+              </TableCell>
             </TableRow>
-          </TableHeader>
-          <TableBod>
-            {rows.map((row, index) => (
-              <TableRow key={index}>
-                <TableCell>{row.txFileName}</TableCell>
-                <TableCell>
-                  {moment(row.dtProcess).format('DD/MM/YYYY HH:MM')}
-                </TableCell>
-                <TableCell>{row.userCreation}</TableCell>
-                <TableCell>{capitalizeFirstLetter(row.tariffType)}</TableCell>
-                <TableCell>{capitalizeFirstLetter(row.tariffModalType)}</TableCell>
-                {/* eslint-disable-next-line @typescript-eslint/strict-boolean-expressions */}
-                <TableCell>{row.nmAgent && capitalizeFirstLetter(row.nmAgent)}</TableCell>
-                <TableCell>
-                  <div
-                    style={{
-                      borderRadius: '50%',
-                      height: '15px',
-                      width: '15px',
-                      float: 'left',
-                      marginRight: '5px',
-                      backgroundColor: `${chooseStatusColor(row.txStatus)}`
-                    }}>
-                  </div>
-                  <div
-                    style={{
-                      lineHeight: '16px',
-                      marginLeft: '10px',
-                      cursor: 'pointer'
-                    }}
-                    onClick={
-                      row.txStatus ===
-                      I18n.t('pages.tariff.tariffTableProcessing.sucessLabel')
-                        ? () => {}
-                        : (e) => handleClick(e, index, row.idUploadFile)
-                    }
-                  >
-                    {capitalizeFirstLetter(row.txStatus)}
-                  </div>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBod>
-        </Table>
-        <PaginationContainer>
-          <PaginationMainContainer>
-            <Pagination
-              count={totalFilesList}
-              labelDisplay="exibindo"
-              labelDisplayedRows="de"
-              labelRowsPerPage="Arquivos por página"
-              onPageChange={(value) =>
-                setFilter((filter: any) => ({ ...filter, page: value }))
-              }
-              onRowsPerPageChange={(value) =>
-                setFilter((filter: any) => ({
-                  ...filter,
-                  size: value,
-                  page: 0
-                }))
-              }
-              tooltipBack="Anterior"
-              tooltipFirst="Primeira"
-              tooltipLast="Última"
-              tooltipNext="Próxima"
-            />
-          </PaginationMainContainer>
-        </PaginationContainer>
-      </TableContainer>
+          ))}
+        </TableBod>
+      </Table>
     </>
   )
 }
