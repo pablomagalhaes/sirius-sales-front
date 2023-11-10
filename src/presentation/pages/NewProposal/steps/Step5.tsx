f6160afc489751ec9f24ddc6409ef8cb79f8e7fd
Thiago Magalhães <pablo.magalhaes@infobase.com.br>
2023-11-10 18:22:03 -0300

    bugfix/SIRPLT-1743 - Valores de Moeda de venda e Valor de venda não são exibidos ao editar uma proposta

--- src/presentation/pages/NewProposal/steps/Step5.tsx
+++ src/presentation/pages/NewProposal/steps/Step5.tsx
@@ -206,13 +206,13 @@ const Step5 = ({
   const getValueSale = (): string[] => {
     return proposal.agents.map((agent) => {
       return proposal.costs
-        .find(cost => cost.costType === CostTypes.Freight && cost.valuePurchase === null && cost.agent?.idBusinessPartnerAgent === agent.idBusinessPartnerAgent)?.valueSale?.toFixed(2) ?? ''
+        .find(cost => cost.costType === CostTypes.Freight && cost.agent?.idBusinessPartnerAgent === agent.idBusinessPartnerAgent)?.valueSale?.toFixed(2) ?? ''
     })
   }
 
   const [dataSales, setDataSales] = useState<any>({
     idCost: proposal.costs.find(cost => cost.costType === CostTypes.Freight && (cost.agent === null || cost.agent.idBusinessPartnerAgent === null))?.idCost ?? null,
-    currencySale: String(proposal.costs.find(cost => cost.costType === CostTypes.Freight && (cost.valuePurchase === null))?.idCurrencySale ?? ''),
+    currencySale: String(proposal.costs.find(cost => cost.costType === CostTypes.Freight)?.idCurrencySale ?? ''),
     valueSale: getValueSale()
   })
 
@@ -1592,8 +1592,8 @@ const Step5 = ({
                                   handleContainerChange(newData, 'valueSale', e.target.value, index, true)
                                 }}
                                 toolTipTitle={I18n.t('components.itemModal.requiredField')}
-                                invalid={invalidInput && inputValidation(dataContainer[index].valueSale)}
-                                value={dataContainer[index].valueSale}
+                                invalid={invalidInput && inputValidation(dataSales[index].valueSale)}
+                                value={dataSales[index].valueSale}
                                 variant='outlined'
                                 size='small'
                               />
