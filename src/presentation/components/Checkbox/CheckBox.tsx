e56992532eafe741373ca2d72d77e8b387b1c5a9
Mario Costa <mariocosta@fiorde.com.br>
2024-01-10 19:00:27 -0300

    SIRPLT-1791 checkbox component created

---
+++ src/presentation/components/Checkbox/CheckBox.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import {
+  FormControlLabel,
+  Checkbox
+} from '@material-ui/core/'
+
+interface CheckBoxProps {
+  theme?: any
+  checked: boolean
+  label: string
+  handleChange: Function
+  value: string
+  id: string
+}
+
+const CheckBox: React.FC<CheckBoxProps> = ({
+  theme,
+  checked,
+  label,
+  handleChange,
+  value,
+  id
+}) => {
+  // eslint-disable-next-line @typescript-eslint/explicit-function-return-type
+  const handleOnChange = (item): void => {
+    handleChange(item) 
+  }
+  return (
+    <FormControlLabel
+      value={value}
+      control={
+        <Checkbox
+          checked={checked}
+          onChange={handleOnChange}
+          data-testid="checkbox-test"
+        />
+      }
+      label={label}
+      id={id}
+      data-testid="checkbox-label-test"
+    />
+  )
+}
+
+export default CheckBox
